866cfcdeaa7b11323a5c558d94cba10acd9815fe
TySuvion <80416049+TySuvion@users.noreply.github.com>
2025-04-29 18:42:15 +0200

    cleared the project cause i will redo for auth flow

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { UserModule } from './user/user.module';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), UserModule, AuthModule],
+  imports: [ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/user/dto/create-user.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsString, Length } from 'class-validator';
-
-export class CreateUserDto {
-  @IsString()
-  @Length(3, 15)
-  username: string;
-}
