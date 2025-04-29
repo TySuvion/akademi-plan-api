bcb60e002323118a8968bc1065198650064bf6f5
TySuvion <80416049+TySuvion@users.noreply.github.com>
2025-04-29 19:24:00 +0200

    generated courses resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
+import { CoursesModule } from './courses/courses.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), PrismaModule],
+  imports: [ConfigModule.forRoot(), PrismaModule, CoursesModule],
   controllers: [AppController],
   providers: [AppService],
 })
