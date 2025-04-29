5bb30b5dc124dd722ac4bbd64732983064b24e3f
TySuvion <80416049+TySuvion@users.noreply.github.com>
2025-04-29 19:23:16 +0200

    set up swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { PrismaClientExceptionFilter } from './prisma-exception.filter';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Course API')
+    .setDescription('The course API description')
+    .setVersion('0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -14,7 +23,7 @@ async function bootstrap() {
     }),
   );
 
-  app.useGlobalFilters(new PrismaClientExceptionFilter());
+  app.useGlobalFilters();
 
   await app.listen(process.env.PORT ?? 3000);
 }
