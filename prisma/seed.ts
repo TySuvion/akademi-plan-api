7b845556190a3ab1182ab9bd54412a5948512a08
TySuvion <80416049+TySuvion@users.noreply.github.com>
2025-05-01 17:16:42 +0200

    added users in seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,16 +3,44 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 async function main() {
-  // create two dummy courses
-  const course1 = await prisma.course.create({
+  // create two dummy users
+  const user1 = await prisma.user.create({
     data: {
-      name: 'Mathe 1',
+      username: 'TestUser1',
+      password: 'TestPassword1',
     },
   });
 
-  const course2 = await prisma.course.create({
+  const user2 = await prisma.user.create({
     data: {
-      name: 'Statistik',
+      username: 'TestUser2',
+      password: 'TestPassword2',
+    },
+  });
+
+  // create two dummy courses
+  const course1 = await prisma.course.update({
+    where: {
+      id: 1,
+    },
+    data: {
+      userID: user1.id,
+    },
+  });
+
+  const course2 = await prisma.course.update({
+    where: {
+      id: 2,
+    },
+    data: {
+      userID: user2.id,
+    },
+  });
+
+  const course3 = await prisma.course.create({
+    data: {
+      name: 'Grundlagen der Informatik',
+      userID: user1.id,
     },
   });
 
